b659dd19f400a0517779f3ad2d9d242dfbeebee9
taekyeongwon <tkw3351@gmail.com>
2024-11-21 10:09:37 +0900

    프로그레스바 정리 다이얼로그 + 프로그레스 테스트

--- MyApplication/app/src/main/java/com/example/myapplication/ContentProgress.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ContentProgress.kt
@@ -10,14 +10,13 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.rememberTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -26,29 +25,26 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.text.drawText
-import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
-fun ContentProgress() {
-
+fun InfiniteProgress() {
     val infTransition = rememberInfiniteTransition(label = "transition")
     val translateAnimation by infTransition.animateFloat(
         initialValue = 360f,
         targetValue = 0f,
         animationSpec = infiniteRepeatable(
             animation = tween(
-                durationMillis = 3000,
+                durationMillis = 1000,
                 easing = LinearEasing
             ),
             repeatMode = RepeatMode.Restart
         ), label = "animation"
     )
-    val textMeasurer = rememberTextMeasurer()
-    var remainTime by remember { mutableStateOf(3) }
+//    val textMeasurer = rememberTextMeasurer()
+//    val remainTime by remember { mutableStateOf(3) }
 
     Canvas (modifier = Modifier.size(60.dp)) {
         val startAngle = 5f
@@ -73,26 +69,105 @@ fun ContentProgress() {
             )
         }
 
-        val textSize = density.run {
-            textMeasurer.measure(remainTime.toString()).size
-        }
-        drawText(
-            textMeasurer,
-            text = remainTime.toString(),
-            topLeft = Offset(center.x - textSize.width / 2, center.y - textSize.height / 2)
-        )
+//        val textSize = density.run {
+//            textMeasurer.measure(remainTime.toString()).size
+//        }
+//        drawText(
+//            textMeasurer,
+//            text = remainTime.toString(),
+//            topLeft = Offset(center.x - textSize.width / 2, center.y - textSize.height / 2)
+//        )
     }
+}
+
+@Composable
+fun FiniteProgress(degree: Float, modifier: Modifier = Modifier) {
+    Canvas (modifier = modifier) {
+        val strokeSize = 6.dp.toPx()
+
+        rotate(-90f) {
+            drawArc(
+                color = Color.Blue,
+                size = Size(size.width - strokeSize, size.width - strokeSize),
+                startAngle = -strokeSize / 2,
+                sweepAngle = degree,
+                useCenter = false,
+                topLeft = Offset(strokeSize / 2, strokeSize / 2),
+                style = Stroke(width = strokeSize, cap = StrokeCap.Round)
+            )
+        }
+    }
+}
+
+@Composable
+fun FiniteGradientProgress(degree: Float, modifier: Modifier = Modifier) {
+    Canvas (modifier = modifier) {
+        val strokeSize = 6.dp.toPx()
+
+        rotate(-90f) {
+            drawArc(
+                brush = Brush.sweepGradient(
+                    colors = listOf(
+                        Color.Blue,
+                        Color.Transparent
+                    ),
+                    center = Offset(size.width / 2f, size.height / 2f)
+                ),
+                size = Size(size.width - strokeSize, size.width - strokeSize),
+                startAngle = -strokeSize / 2,
+                sweepAngle = degree,
+                useCenter = false,
+                topLeft = Offset(strokeSize / 2, strokeSize / 2),
+                style = Stroke(width = strokeSize, cap = StrokeCap.Round)
+            )
+        }
+    }
+}
+
+@Composable
+fun ContentProgress(modifier: Modifier = Modifier, initialValue: Int = 3, completeListener: () -> Unit) {
+    val remainProgress = remember { MutableTransitionState(-360f) }
+    remainProgress.targetState = 0f
+    val progressTransition = rememberTransition(remainProgress)
+    val remainProgressAnimateValue by progressTransition.animateFloat(
+        transitionSpec = {
+            tween(
+                durationMillis = initialValue * 1000,
+                easing = LinearEasing
+            )
+        }, label = ""
+    ) {
+        it
+    }
+
+    val remainTime = remember {MutableTransitionState(initialValue + 1)}
+    remainTime.targetState = 1
+    val transition = rememberTransition(remainTime)
+    val remainTimeAnimateValue by transition.animateInt(
+        transitionSpec = {
+            tween(
+                durationMillis = initialValue * 1000,
+                easing = LinearEasing
+            )
+        }, label = ""
+    ) {
+        if(remainProgressAnimateValue == remainProgress.targetState) {
+            completeListener()
+            0
+        } else {
+            it
+        }
+    }
+
+    Box(contentAlignment = Alignment.Center) {
+        FiniteProgress(remainProgressAnimateValue, modifier = modifier)
+        Text(remainTimeAnimateValue.toString())
+    }
+
 }
 
 @Preview
 @Composable
-fun TestProgress() {
-    val currentState = remember {MutableTransitionState(4)}
-    currentState.targetState = 1
-    val transition = rememberTransition(currentState)
-    val translateValue by transition.animateInt(
-        transitionSpec = { tween(durationMillis = 3000, easing = LinearEasing) }, label = ""
-    ) { it }
-
-    Text(translateValue.toString())
+fun ContentProgressPreview() {
+    ContentProgress(modifier = Modifier.size(60.dp), 5, {})
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.AnimationSpec
-import androidx.compose.animation.core.DecayAnimationSpec
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.rememberSplineBasedDecay
-import androidx.compose.animation.splineBasedDecay
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.AnchoredDraggableState
@@ -17,6 +13,7 @@ import androidx.compose.foundation.gestures.DraggableAnchors
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.anchoredDraggable
 import androidx.compose.foundation.gestures.animateTo
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,13 +22,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -44,7 +41,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,6 +49,7 @@ import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntOffset
@@ -118,7 +115,6 @@ class MainActivity : ComponentActivity() {
                         Button(onClick = {openDialog = true}) {
 
                         }
-                        ContentProgress()
                     }
                 }
             }
@@ -136,76 +132,105 @@ fun CustomDialog(
     expanded: Boolean = false,
     setExpanded: (Boolean) -> Unit,
     minHeight: Float = 0f,
-    setHeight: (Float, Float) -> Unit,
+    setHeight: (Float, Float) -> Unit
 ) {
-    val scope = rememberCoroutineScope()
-
     Dialog(onDismissRequest = {onDismissRequest()}) {
         DialogContent(
             state = state,
             minHeight = minHeight,
             setHeight = setHeight,
             header = {
-                Box(modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(),
-                    contentAlignment = Alignment.CenterStart) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(
-                            imageVector = Icons.Default.Info,
-                            contentDescription = null
-                        )
-                        Spacer(modifier = Modifier.width(16.dp))
-                        Text(
-                            text = "This is a minimal dialog",
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                    }
-
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Button(onClick = {
-                            setChangingText("qwerwqer\nqwerqwerqwer\nqwerqwerwerqwer\nqwerqwerqwer")
-                        }) { }
-                        CircularProgressIndicator()
-                    }
-
-                }
+                DialogHeader(setChangingText, onDismissRequest)
             },
             content = {
-                Spacer(modifier = Modifier
-                    .height(8.dp)
-                    .padding(start = 16.dp, end = 16.dp))
-                Text(
-                    text = changingText,
-                    textAlign = TextAlign.Justify,
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-                )
+                DialogBody(changingText)
             },
             bottom = {
-                HorizontalDivider(
-                    modifier = Modifier
-                        .width(180.dp)
-//                                .align(Alignment.CenterHorizontally)
-                        .padding(10.dp)
-                        .anchoredDraggable(
-                            state = state, orientation = Orientation.Vertical
-                        )
-                        .clickable {
-                            scope.launch {
-                                setExpanded(!expanded)
-                                if (expanded) {
-                                    state.animateTo(DragValue.End)
-                                } else {
-                                    state.animateTo(DragValue.Start)
-                                }
-                            }
-
-                        },
-                    color = Color.Black
-                )
+                DialogBottom(state, expanded, setExpanded)
             }
         )
     }
+}
+
+@Composable
+fun DialogHeader(
+    setChangingText: (String) -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    Row(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.weight(1f)) {
+            Icon(
+                imageVector = Icons.Default.Info,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = "This is a minimal dialog asdf",
+                style = MaterialTheme.typography.bodySmall,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+                //텍스트가 가려질 때 처럼 콘텐츠 가려질 때 처리
+            )
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End) {
+            Button(onClick = {
+                setChangingText("qwerwqer\nqwerqwerqwer\nqwerqwerwerqwer\nqwerqwerqwer")
+            }) { }
+            ContentProgress(Modifier.size(60.dp), 5) {
+                onDismissRequest()
+            } //프로그레스 끝날 때 리스너 받기
+        }
+
+    }
+}
+
+@Composable
+fun DialogBody(changingText: String = "") {
+    Spacer(modifier = Modifier
+        .height(8.dp)
+        .padding(start = 16.dp, end = 16.dp))
+    Text(
+        text = changingText,
+        textAlign = TextAlign.Justify,
+        modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DialogBottom(
+    state: AnchoredDraggableState<DragValue>,
+    expanded: Boolean = false,
+    setExpanded: (Boolean) -> Unit = {},
+) {
+    val scope = rememberCoroutineScope()
+
+    HorizontalDivider(
+        modifier = Modifier
+            .width(180.dp)
+//                                .align(Alignment.CenterHorizontally)
+            .padding(10.dp)
+            .anchoredDraggable(
+                state = state, orientation = Orientation.Vertical
+            )
+            .clickable {
+                scope.launch {
+                    setExpanded(!expanded)
+                    if (expanded) {
+                        state.animateTo(DragValue.End)
+                    } else {
+                        state.animateTo(DragValue.Start)
+                    }
+                }
+
+            },
+        color = Color.Black
+    )
 }
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -290,30 +315,11 @@ fun DialogContent(
     }
 }
 
-
-
-@Preview(widthDp = 460)
+@Preview(widthDp = 260)
 @Composable
 fun HeaderPreview() {
-    Box(modifier = Modifier
-        .padding(16.dp)
-        .fillMaxWidth(),
-        contentAlignment = Alignment.CenterStart) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(
-                imageVector = Icons.Default.Info,
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(
-                text = "This is a minimal dialog",
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
-
-        Button(
-            onClick = {},
-            modifier = Modifier.align(Alignment.CenterEnd)
-        ) { }
-    }
+    DialogHeader(
+        setChangingText = {},
+        onDismissRequest = {}
+    )
 }

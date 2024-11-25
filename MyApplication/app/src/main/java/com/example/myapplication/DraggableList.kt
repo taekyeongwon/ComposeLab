56cd124c91602d100b345e0207942f114d42e2b4
taekyeongwon <tkw3351@gmail.com>
2024-11-25 11:57:51 +0900

    스크롤 시 앵커 드래그 Modifier로 분리

--- MyApplication/app/src/main/java/com/example/myapplication/DraggableList.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/DraggableList.kt
@@ -1,42 +1,26 @@
 package com.example.myapplication
 
-import android.util.Log
-import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.rememberSplineBasedDecay
-import androidx.compose.animation.splineBasedDecay
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.AnchoredDraggableState
 import androidx.compose.foundation.gestures.DraggableAnchors
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.anchoredDraggable
 import androidx.compose.foundation.gestures.animateTo
-import androidx.compose.foundation.gestures.awaitDragOrCancellation
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.awaitTouchSlopOrCancellation
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.foundation.gestures.drag
-import androidx.compose.foundation.gestures.scrollBy
-import androidx.compose.foundation.gestures.stopScroll
-import androidx.compose.foundation.gestures.verticalDrag
-import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -48,22 +32,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.RequestDisallowInterceptTouchEvent
-import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.pointerInteropFilter
-import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlin.math.abs
 import kotlin.math.roundToInt
 
 @Preview
@@ -99,48 +74,10 @@ fun DraggableList(state: AnchoredDraggableState<DragValue>) {
     val scope = rememberCoroutineScope()
     var expanded by remember { mutableStateOf(false) }
     var scrollEnabled by remember { mutableStateOf(true) }
-    var isScrollDown by remember { mutableStateOf(false) }
     Button(onClick = {
         expanded = !expanded
     }) { }
     val scrollState = rememberLazyListState()
-
-    val nestedScrollConnection = object : NestedScrollConnection {
-//        override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-//
-//            scope.launch {
-////                scrollState.dispatchRawDelta(0f)
-////                scrollState.dispatchRawDelta(available.y)
-//                state.dispatchRawDelta(available.y)
-//            }
-//
-//            println("Received onPreScroll callback. ${available}")
-//            return Offset.Zero
-//        }
-
-        override fun onPostScroll(
-            consumed: Offset,
-            available: Offset,
-            source: NestedScrollSource
-        ): Offset {
-            println("Received onPostScroll callback. $consumed $available")
-            scope.launch {
-//                scrollState.dispatchRawDelta(0f)
-
-                state.dispatchRawDelta(consumed.y)
-            }
-            return Offset.Zero
-        }
-    }
-
-//    SideEffect {
-//        Log.d("test", scrollState.canScrollForward.toString())
-//        Log.d("test", scrollState.canScrollBackward.toString())
-////        scrollState.stopScroll()
-//        scrollState.interactionSource.interactions.collect {
-//            Log.d("test", it.toString())
-//        }
-//    }
 
     if(expanded) {
         Box(modifier = Modifier) {
@@ -157,56 +94,9 @@ fun DraggableList(state: AnchoredDraggableState<DragValue>) {
                                 .toDp()
                         }
                     )
-                    .pointerInput(Unit) {
-                        awaitPointerEventScope {
-                            while (true) {
-                                val event = awaitPointerEvent()
-
-                                //pointer pass 종류에 따라 뭐가 다른지. 일단 이 changes에서 현재 마우스 포인터의 포지션을 알아올 수 있으므로
-                                //해당 포지션이 scrollState.layoutInfo의 가장 하단에 닿았을 때 변경되는 값 만큼 anchor state를
-                                //변경해주면 될 것 같음!
-                                when(event.type) {
-                                    PointerEventType.Move -> {
-                                        if(event.changes.isNotEmpty()) {
-                                            val currentPositionY =
-                                                event.changes[event.changes.size - 1].position.y
-                                            val scrollEndOffset =
-                                                scrollState.layoutInfo.viewportEndOffset -
-                                                        with(density) { 10.dp.toPx() }
-//                                            val scrollEndOffsetPx =
-//                                                with(density) { 40.dp.toPx() }  //선택 영역 범위 제한
-
-//                                            if (abs(currentPositionY - scrollEndOffset) < scrollEndOffsetPx) {
-//                                                scrollEnabled = false
-//                                                state.dispatchRawDelta(currentPositionY - scrollEndOffset)
-//                                            }
-
-                                            val lastPositionY = event.changes[event.changes.size - 1].previousPosition.y
-
-                                            //아래로 빠르게 드래그하는 경우 prev 600 current 800 endoffset 788
-                                            //위로 빠르게 드래그하는 경우 prev 800 current 600 endoffset 788
-                                            if(lastPositionY < currentPositionY && currentPositionY > scrollEndOffset) {
-                                                scrollEnabled = false
-                                                isScrollDown = true
-                                                state.dispatchRawDelta(currentPositionY - scrollEndOffset)
-                                            } else if(lastPositionY >= currentPositionY && currentPositionY < scrollEndOffset && isScrollDown) {
-                                                scrollEnabled = false
-                                                state.dispatchRawDelta(currentPositionY - scrollEndOffset)
-                                            }
-                                        }
-                                    }
-                                    PointerEventType.Release -> {
-                                        scrollEnabled = true
-                                        isScrollDown = false
-                                        scope.launch {
-                                            state.settle(state.lastVelocity)
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                    .scrollWithAnchor(scrollState, state) { isScrolling ->
+                        scrollEnabled = !isScrolling
                     }
-
             ) {
                 items(20) {
                     Text(text = "Item $it", modifier = Modifier.padding(16.dp))
@@ -246,3 +136,59 @@ fun DraggableList(state: AnchoredDraggableState<DragValue>) {
 
     }
 }
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun Modifier.scrollWithAnchor(
+    scrollState: LazyListState,
+    anchoredDraggableState: AnchoredDraggableState<DragValue>,
+    onScrolling: (Boolean) -> Unit
+): Modifier {
+    val density = LocalDensity.current
+    val scope = rememberCoroutineScope()
+    var isScrollDown by remember { mutableStateOf(false) }  //아래로 앵커가 드래그 된 이후에만 위로 앵커를 올릴 수 있도록
+
+    return this then Modifier.pointerInput(Unit) {
+        awaitPointerEventScope {
+            while (true) {
+                val event = awaitPointerEvent()
+
+                //pointer pass 종류에 따라 뭐가 다른지. 일단 이 changes에서 현재 마우스 포인터의 포지션을 알아올 수 있으므로
+                //해당 포지션이 scrollState.layoutInfo의 가장 하단에 닿았을 때 변경되는 값 만큼 anchor state를
+                //변경해주면 될 것 같음!
+                when(event.type) {
+                    PointerEventType.Move -> {
+                        if(event.changes.isNotEmpty()) {
+                            val currentPositionY =
+                                event.changes[event.changes.size - 1].position.y
+                            val scrollEndOffset =
+                                scrollState.layoutInfo.viewportEndOffset -
+                                        with(density) { 10.dp.toPx() }
+
+                            val lastPositionY = event.changes[event.changes.size - 1].previousPosition.y
+
+                            //아래로 빠르게 드래그하는 경우 prev 600 current 800 endoffset 788
+                            //위로 빠르게 드래그하는 경우 prev 800 current 600 endoffset 788
+                            if(lastPositionY < currentPositionY && currentPositionY > scrollEndOffset) {
+                                isScrollDown = true
+                                onScrolling(true)
+                                anchoredDraggableState.dispatchRawDelta(currentPositionY - scrollEndOffset)
+                            } else if(lastPositionY >= currentPositionY && currentPositionY < scrollEndOffset && isScrollDown) {
+                                onScrolling(true)
+                                anchoredDraggableState.dispatchRawDelta(currentPositionY - scrollEndOffset)
+                            }
+                        }
+                    }
+                    PointerEventType.Release -> {
+                        isScrollDown = false
+                        onScrolling(false)
+                        scope.launch {
+                            anchoredDraggableState.settle(anchoredDraggableState.lastVelocity)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
